bf1df7854d311b91cc517e3b559e633ae6951320
Ali Hussain Khan <36451371+ahk4815@users.noreply.github.com>
2025-05-23 19:16:19 +0000

    Add sample image

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-// function MyFunc(){
-//   return <h1>This is React</h1>
-// }
+function MyFunc(){
+  return <h1>This is React</h1>
+}
 
-// const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 
-// root.render(
-//   <StrictMode>
-//     <MyFunc />
-//   </StrictMode>,
-// )
+root.render(
+  <main>
+    <img src="/src/assets/IceCream.jpeg"/>
+    <h1>This is another element</h1>
+  </main>
+)
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is impreative coding"
-h1.className = "header"
-document.getElementById("root").appendChild(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is impreative coding"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1)
